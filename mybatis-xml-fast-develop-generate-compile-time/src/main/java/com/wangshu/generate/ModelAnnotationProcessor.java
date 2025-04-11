16bf0b667e543815f2f9bf3e53f25813f8faca11
wangshu <2560334673@qq.com>
2025-04-11 19:10:21 +0800

    引入编译期生成配置，引入 @ForceTrigger 强制触发生成，弥补开发中结构分层问题，对于多模块分层更友好，编译期生成逻辑代码优化

--- mybatis-xml-fast-develop-generate-compile-time/src/main/java/com/wangshu/generate/ModelAnnotationProcessor.java
+++ mybatis-xml-fast-develop-generate-compile-time/src/main/java/com/wangshu/generate/ModelAnnotationProcessor.java
@@ -23,15 +23,19 @@ package com.wangshu.generate;
 // SOFTWARE.
 
 import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.lang.Dict;
+import cn.hutool.core.util.ClassUtil;
 import cn.hutool.core.util.StrUtil;
-import cn.hutool.setting.yaml.YamlUtil;
 import com.google.auto.service.AutoService;
 import com.wangshu.annotation.Model;
+import com.wangshu.base.model.BaseModel;
 import com.wangshu.exception.MessageException;
+import com.wangshu.generate.config.GenerateConfig;
 import com.wangshu.generate.java.GenerateJavaMMSSCQ;
 import com.wangshu.generate.metadata.field.ColumnElementInfo;
+import com.wangshu.generate.metadata.field.ColumnFieldInfo;
+import com.wangshu.generate.metadata.model.ModelClazzInfo;
 import com.wangshu.generate.metadata.model.ModelElementInfo;
+import com.wangshu.generate.metadata.model.ModelInfo;
 import com.wangshu.generate.metadata.module.ModuleTemplateInfo;
 import com.wangshu.generate.xml.GenerateXml;
 import com.wangshu.generate.xml.GenerateXmlMssql;
@@ -51,9 +55,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.Writer;
-import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.Consumer;
@@ -61,7 +63,7 @@ import java.util.function.Consumer;
 /**
  * @author wangshu-g
  */
-@SupportedAnnotationTypes("com.wangshu.annotation.Model")
+@SupportedAnnotationTypes({"com.wangshu.annotation.Model", "com.wangshu.annotation.ForceTrigger"})
 @AutoService(Processor.class)
 public class ModelAnnotationProcessor extends AbstractProcessor {
 
@@ -69,16 +71,37 @@ public class ModelAnnotationProcessor extends AbstractProcessor {
     private Filer filer;
     private Elements elementUtils;
     private Types typeUtils;
-
-    private final URL path = ModelAnnotationProcessor.class.getClassLoader().getResource("");
+    private Consumer<MessageException> messageExceptionConsumer;
+    private String modulePath;
+    private String moduleName;
+    private GenerateConfig generateConfig;
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
-        this.messager = processingEnv.getMessager();
-        this.filer = processingEnv.getFiler();
-        this.elementUtils = processingEnv.getElementUtils();
-        this.typeUtils = processingEnv.getTypeUtils();
+        messager = processingEnv.getMessager();
+        filer = processingEnv.getFiler();
+        elementUtils = processingEnv.getElementUtils();
+        typeUtils = processingEnv.getTypeUtils();
+        messageExceptionConsumer = (MessageException message) -> {
+            switch (message.getMessageType()) {
+                case ERROR:
+                    printError(message.getMessage());
+                    break;
+                case WARN:
+                    printWarn(message.getMessage());
+                    break;
+                case NOTE:
+                    printNote(message.getMessage());
+                    break;
+            }
+        };
+        String moduleClassesPath = Objects.requireNonNull(ModelAnnotationProcessor.class.getClassLoader().getResource("")).getPath();
+        modulePath = moduleClassesPath.replace("target/classes/", "");
+        String[] dirNamePath = modulePath.replaceFirst("/", "").split("/");
+        moduleName = dirNamePath[dirNamePath.length - 1];
+        String applicationYmlFilePath = StrUtil.concat(false, moduleClassesPath, File.separator, "application.yml");
+        generateConfig = new GenerateConfig(applicationYmlFilePath);
     }
 
     @Override
@@ -90,155 +113,146 @@ public class ModelAnnotationProcessor extends AbstractProcessor {
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public boolean process(Set<? extends TypeElement> annotations, @NotNull RoundEnvironment roundEnv) {
         if (roundEnv.processingOver()) {
             return true;
         }
         try {
-            Consumer<MessageException> messageExceptionConsumer = (MessageException message) -> {
-                switch (message.getMessageType()) {
-                    case ERROR:
-                        this.printError(message.getMessage());
-                        break;
-                    case WARN:
-                        this.printWarn(message.getMessage());
-                        break;
-                    case NOTE:
-                        this.printNote(message.getMessage());
-                        break;
-                }
-            };
-            assert this.path != null;
-            String classesPath = this.path.getPath();
-            String applicationYmlFilePath = StrUtil.concat(false, classesPath, File.separator, "application.yml");
-            boolean xml = true;
-            boolean mapper = true;
-            boolean service = true;
-            boolean serviceImpl = true;
-            boolean controller = true;
-            boolean query = true;
-            if (FileUtil.exist(applicationYmlFilePath)) {
-                Dict dict = YamlUtil.loadByPath(applicationYmlFilePath);
-                xml = dict.get("mybatis-xml-fast-develop.generate-compile-time.xml", true);
-                mapper = dict.get("mybatis-xml-fast-develop.generate-compile-time.mapper", true);
-                service = dict.get("mybatis-xml-fast-develop.generate-compile-time.service", true);
-                serviceImpl = dict.get("mybatis-xml-fast-develop.generate-compile-time.serviceImpl", true);
-                controller = dict.get("mybatis-xml-fast-develop.generate-compile-time.controller", true);
-                query = dict.get("mybatis-xml-fast-develop.generate-compile-time.query", true);
-            }
-            String modulePath = classesPath.replace("target/classes/", "");
-            String[] dirNamePath = modulePath.replaceFirst("/", "").split("/");
-            String moduleName = dirNamePath[dirNamePath.length - 1];
             ModuleTemplateInfo moduleTemplateInfo = null;
-            for (Element element : roundEnv.getElementsAnnotatedWith(Model.class)) {
-                if (element instanceof TypeElement temp) {
-                    if (Objects.isNull(moduleTemplateInfo)) {
-                        String modulePackageName = element.asType().toString().replace(StrUtil.concat(false, ".model.", element.getSimpleName().toString()), "");
-                        moduleTemplateInfo = new ModuleTemplateInfo(moduleName, modulePackageName, modulePath);
-                        FileUtil.del(moduleTemplateInfo.getModuleGeneratePath());
-                    }
-                    Model model = element.getAnnotation(Model.class);
-                    if (Objects.nonNull(model)) {
-                        ModelElementInfo modelElementInfo = new ModelElementInfo(moduleTemplateInfo, temp, this.typeUtils);
-                        GenerateJavaMMSSCQ<ModelElementInfo, ColumnElementInfo> generateJava = new GenerateJavaMMSSCQ<>(modelElementInfo, messageExceptionConsumer);
-                        GenerateXml<ModelElementInfo, ColumnElementInfo> generateXml = null;
-                        switch (modelElementInfo.getDataBaseType()) {
-                            case mysql -> generateXml = new GenerateXmlMysql<>(modelElementInfo, messageExceptionConsumer);
-                            case postgresql -> generateXml = new GenerateXmlPostgresql<>(modelElementInfo, messageExceptionConsumer);
-                            case mssql -> generateXml = new GenerateXmlMssql<>(modelElementInfo, messageExceptionConsumer);
-                            default -> printError("暂未支持的数据库类型");
+            if (generateConfig.isScanClassFile()) {
+                if (StrUtil.isBlank(generateConfig.getScanClassFileModelPackage())) {
+                    printError("使用 scan-class-file 必须配置 scan-class-file-model-package");
+                } else {
+                    for (Class<?> clazz : ClassUtil.scanPackage(generateConfig.getScanClassFileModelPackage())) {
+                        if (clazz.isAnnotationPresent(Model.class) && BaseModel.class.isAssignableFrom(clazz)) {
+                            if (Objects.isNull(moduleTemplateInfo)) {
+                                String modulePackageName = clazz.getName().replace(StrUtil.concat(false, ".model.", clazz.getSimpleName()), "");
+                                moduleTemplateInfo = new ModuleTemplateInfo(moduleName, modulePackageName, modulePath);
+                                FileUtil.del(moduleTemplateInfo.getModuleGeneratePath());
+                            }
+                            ModelClazzInfo modelClazzInfo = new ModelClazzInfo(moduleTemplateInfo, (Class<? extends BaseModel>) clazz);
+                            GenerateJavaMMSSCQ<ModelClazzInfo, ColumnFieldInfo> generateJava = new GenerateJavaMMSSCQ<>(modelClazzInfo, messageExceptionConsumer);
+                            GenerateXml<ModelClazzInfo, ColumnFieldInfo> generateXml = null;
+                            switch (modelClazzInfo.getDataBaseType()) {
+                                case mysql -> generateXml = new GenerateXmlMysql<>(modelClazzInfo, messageExceptionConsumer);
+                                case postgresql -> generateXml = new GenerateXmlPostgresql<>(modelClazzInfo, messageExceptionConsumer);
+                                case mssql -> generateXml = new GenerateXmlMssql<>(modelClazzInfo, messageExceptionConsumer);
+                                default -> printError("暂未支持的数据库类型");
+                            }
+                            writeGenerateFile(modelClazzInfo, generateJava, generateXml, generateConfig);
                         }
-                        if (Objects.nonNull(generateXml)) {
-                            if (!FileUtil.exist(modelElementInfo.getMapperFilePath()) && mapper) {
-                                generateJava.writeMapper();
-                                this.writeJavaSourceFile(modelElementInfo.getMapperFullName(), generateJava.getMapperCode());
+                    }
+                }
+            } else {
+                for (Element element : roundEnv.getElementsAnnotatedWith(Model.class)) {
+                    if (element instanceof TypeElement temp) {
+                        if (Objects.isNull(moduleTemplateInfo)) {
+                            String modulePackageName = element.asType().toString().replace(StrUtil.concat(false, ".model.", element.getSimpleName().toString()), "");
+                            moduleTemplateInfo = new ModuleTemplateInfo(moduleName, modulePackageName, modulePath);
+                            FileUtil.del(moduleTemplateInfo.getModuleGeneratePath());
+                        }
+                        Model model = element.getAnnotation(Model.class);
+                        if (Objects.nonNull(model)) {
+                            ModelElementInfo modelElementInfo = new ModelElementInfo(moduleTemplateInfo, temp, typeUtils);
+                            GenerateJavaMMSSCQ<ModelElementInfo, ColumnElementInfo> generateJava = new GenerateJavaMMSSCQ<>(modelElementInfo, messageExceptionConsumer);
+                            GenerateXml<ModelElementInfo, ColumnElementInfo> generateXml = null;
+                            switch (modelElementInfo.getDataBaseType()) {
+                                case mysql -> generateXml = new GenerateXmlMysql<>(modelElementInfo, messageExceptionConsumer);
+                                case postgresql -> generateXml = new GenerateXmlPostgresql<>(modelElementInfo, messageExceptionConsumer);
+                                case mssql -> generateXml = new GenerateXmlMssql<>(modelElementInfo, messageExceptionConsumer);
+                                default -> printError("暂未支持的数据库类型");
                             }
-                            if (!FileUtil.exist(modelElementInfo.getServiceFilePath()) && service) {
-                                generateJava.writeService();
-                                this.writeJavaSourceFile(modelElementInfo.getServiceFullName(), generateJava.getServiceCode());
-                            }
-                            if (!FileUtil.exist(modelElementInfo.getServiceImplFilePath()) && serviceImpl) {
-                                generateJava.writeServiceImpl();
-                                this.writeJavaSourceFile(modelElementInfo.getServiceImplFullName(), generateJava.getServiceImplCode());
-                            }
-                            if (!FileUtil.exist(modelElementInfo.getControllerFilePath()) && controller) {
-                                generateJava.writeController();
-                                this.writeJavaSourceFile(modelElementInfo.getControllerFullName(), generateJava.getControllerCode());
-                            }
-                            if (!FileUtil.exist(modelElementInfo.getQueryFilePath()) && query) {
-                                generateJava.writeQuery();
-                                this.writeJavaSourceFile(modelElementInfo.getQueryFullName(), generateJava.getQueryCode());
-                            }
-                            if (!FileUtil.exist(modelElementInfo.getXmlFilePath()) && xml) {
-                                generateXml.writeXml();
-                            }
+                            writeGenerateFile(modelElementInfo, generateJava, generateXml, generateConfig);
                         }
                     }
                 }
             }
             if (Objects.nonNull(moduleTemplateInfo)) {
                 copyFolderToFolder(new File(moduleTemplateInfo.getModuleGeneratePath()).getAbsolutePath(), new File(moduleTemplateInfo.getModulePath()).getAbsolutePath(), false);
-                if (!FileUtil.exist(moduleTemplateInfo.getModuleCompileClassesXmlPath())) {
-                    copyFolderToFolder(new File(moduleTemplateInfo.getModuleGenerateXmlPath()).getAbsolutePath(), new File(moduleTemplateInfo.getModuleCompileClassesXmlPath()).getAbsolutePath(), false);
-                }
+                copyFolderToFolder(new File(moduleTemplateInfo.getModuleGenerateXmlPath()).getAbsolutePath(), new File(moduleTemplateInfo.getModuleCompileClassesXmlPath()).getAbsolutePath(), false);
             }
         } catch (Exception e) {
-            this.printError("生成失败");
-            this.printError(e.getMessage());
-            e.printStackTrace();
+            printError("生成失败");
+            printError(e.getMessage());
             return false;
         }
         return true;
     }
 
-    private void copyFolderToFolder(String folder1, String folder2, boolean coverExistFile) throws IOException {
-        FileUtil.mkdir(folder2);
-        File fileList = new File(folder1);
-        String[] fileNameArr = fileList.list();
-        File temp;
-        if (Objects.isNull(fileNameArr)) {
+    private void writeGenerateFile(ModelInfo<?, ?> modelInfo, GenerateJavaMMSSCQ<?, ?> generateJavaMMSSCQ, GenerateXml<?, ?> generateXml, GenerateConfig generateConfig) throws IOException {
+        if (Objects.nonNull(generateXml)) {
+            if (!FileUtil.exist(modelInfo.getMapperFilePath()) && generateConfig.isMapper()) {
+                generateJavaMMSSCQ.writeMapper();
+                writeJavaSourceFile(modelInfo.getMapperFullName(), generateJavaMMSSCQ.getMapperCode());
+            }
+            if (!FileUtil.exist(modelInfo.getServiceFilePath()) && generateConfig.isService()) {
+                generateJavaMMSSCQ.writeService();
+                writeJavaSourceFile(modelInfo.getServiceFullName(), generateJavaMMSSCQ.getServiceCode());
+            }
+            if (!FileUtil.exist(modelInfo.getServiceImplFilePath()) && generateConfig.isServiceImpl()) {
+                generateJavaMMSSCQ.writeServiceImpl();
+                writeJavaSourceFile(modelInfo.getServiceImplFullName(), generateJavaMMSSCQ.getServiceImplCode());
+            }
+            if (!FileUtil.exist(modelInfo.getControllerFilePath()) && generateConfig.isController()) {
+                generateJavaMMSSCQ.writeController();
+                writeJavaSourceFile(modelInfo.getControllerFullName(), generateJavaMMSSCQ.getControllerCode());
+            }
+            if (!FileUtil.exist(modelInfo.getQueryFilePath()) && generateConfig.isQuery()) {
+                generateJavaMMSSCQ.writeQuery();
+                writeJavaSourceFile(modelInfo.getQueryFullName(), generateJavaMMSSCQ.getQueryCode());
+            }
+            if (!FileUtil.exist(modelInfo.getXmlFilePath()) && generateConfig.isXml()) {
+                generateXml.writeXml();
+            }
+        }
+    }
+
+    private void copyFolderToFolder(String sourceDir, String targetDir, boolean overwrite) throws IOException {
+        File targetFolder = new File(targetDir);
+        if (!targetFolder.exists()) {
+            boolean ignore = targetFolder.mkdirs();
+        }
+        File sourceFolder = new File(sourceDir);
+        File[] files = sourceFolder.listFiles();
+        if (files == null) {
             return;
         }
-        for (String fileName : fileNameArr) {
-            if (folder1.endsWith(File.separator)) {
-                temp = new File(StrUtil.concat(false, folder1, fileName));
-            } else {
-                temp = new File(StrUtil.concat(false, folder1, File.separator, fileName));
-            }
-            if (temp.isFile()) {
-                File file = new File(StrUtil.concat(false, folder2, File.separator, temp.getName()));
-                if (!file.exists() || coverExistFile) {
-                    Files.copy(Path.of(temp.getAbsolutePath()), new FileOutputStream(file));
+        for (File sourceFile : files) {
+            File targetFile = new File(targetFolder, sourceFile.getName());
+            if (sourceFile.isFile()) {
+                if (!targetFile.exists() || overwrite) {
+                    try (FileOutputStream out = new FileOutputStream(targetFile)) {
+                        Files.copy(sourceFile.toPath(), out);
+                    }
                 }
-            } else if (temp.isDirectory()) {
-                copyFolderToFolder(StrUtil.concat(false, folder1, File.separator, fileName), StrUtil.concat(false, folder2, File.separator, fileName), coverExistFile);
+            } else if (sourceFile.isDirectory()) {
+                copyFolderToFolder(sourceFile.getAbsolutePath(), targetFile.getAbsolutePath(), overwrite);
             }
         }
     }
 
     private void writeJavaSourceFile(String classFullName, String code) throws IOException {
-        JavaFileObject sourceFile = this.filer.createSourceFile(classFullName);
+        JavaFileObject sourceFile = filer.createSourceFile(classFullName);
         try (Writer writer = sourceFile.openWriter()) {
             writer.write(code);
         }
     }
 
     private void printNote(String message) {
-        this.printMessage(Diagnostic.Kind.NOTE, message);
+        printMessage(Diagnostic.Kind.NOTE, message);
     }
 
     private void printWarn(String message) {
-        this.printMessage(Diagnostic.Kind.WARNING, message);
+        printMessage(Diagnostic.Kind.WARNING, message);
     }
 
     private void printError(String message) {
-        this.printMessage(Diagnostic.Kind.ERROR, message);
+        printMessage(Diagnostic.Kind.ERROR, message);
     }
 
     private void printMessage(Diagnostic.Kind kind, String message) {
-        if (Objects.nonNull(message)) {
-            this.messager.printMessage(kind, message);
-        }
+        messager.printMessage(kind, message);
     }
 
 }
