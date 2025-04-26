b1344e1e57c94f4bc8467cdd62f2c6591598a64d
wangshu <2560334673@qq.com>
2025-04-26 16:47:20 +0800

    使用案例

---
+++ mybatis-xml-fast-develop-example/mybatis-xml-fast-develop-multiple-module-example/public-module/data-service/module1-data-service/src/main/java/com/module1/service/Module1Service.java
@@ -0,0 +1,8 @@
+package com.module1.service;
+
+import com.module1.model.Module1;
+import com.wangshu.base.service.BaseDataService;
+import java.lang.Long;
+
+public interface Module1Service extends BaseDataService<Long, Module1> {
+}
