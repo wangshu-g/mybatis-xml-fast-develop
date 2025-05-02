217724f6076166b7d6083b30fffb8d472723ca81
wangshu <2560334673@qq.com>
2025-05-02 21:20:38 +0800

    添加 BaseModelWithDefaultFields 常用字段基类

---
+++ mybatis-xml-fast-develop-core/src/main/java/com/wangshu/base/model/BaseModelWithDefaultFields.java
@@ -0,0 +1,61 @@
+package com.wangshu.base.model;
+
+// MIT License
+//
+// Copyright (c) 2025 2560334673@qq.com wangshu-g https://github.com/wangshu-g/mybatis-xml-fast-develop
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import com.wangshu.annotation.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+
+/**
+ * @author wangshu-g
+ * <p>几个常用的字段</p>
+ */
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+@Data
+public class BaseModelWithDefaultFields<T> extends BaseModel {
+
+    @Column(primary = true)
+    @DefaultOrder
+    private T id;
+
+    @CreatedAt
+    @Column
+    private Date createdAt;
+
+    @UpdatedAt
+    @Column
+    private Date updatedAt;
+
+    @DeletedAt
+    @Column
+    private Date deletedAt;
+
+    @DeleteFlag
+    @Column
+    private Boolean deleteFlag;
+
+}
