729975bb7607ac6d985db03e6000e047e7cfd753
wangshu <2560334673@qq.com>
2025-07-02 19:57:33 +0800

    SoftDelete 模板方法

---
+++ mybatis-xml-fast-develop-core/src/main/java/com/wangshu/base/controller/softdelete/SoftDeleteResultBody.java
@@ -0,0 +1,54 @@
+package com.wangshu.base.controller.softdelete;
+
+// MIT License
+//
+// Copyright (c) 2025 2560334673@qq.com wangshu-g https://github.com/wangshu-g/mybatis-xml-fast-develop
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import com.wangshu.base.controller.BaseDataController;
+import com.wangshu.base.model.BaseModel;
+import com.wangshu.base.result.ResultBody;
+import com.wangshu.base.service.BaseDataService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+/**
+ * @author wangshu-g
+ * <p>软删除</p>
+ **/
+public interface SoftDeleteResultBody<S extends BaseDataService<?, T>, T extends BaseModel> extends BaseDataController<S, T> {
+
+    /**
+     * <p>删除</p>
+     **/
+    @RequestMapping("/softDelete")
+    @ResponseBody
+    public default ResultBody<Object> softDelete(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws Exception {
+        Map<String, Object> map = this.getRequestParams(request);
+        int line = this.getService()._softDelete(map);
+        return line > 0 ? ResultBody.success() : ResultBody.error("记录不存在");
+    }
+
+}
